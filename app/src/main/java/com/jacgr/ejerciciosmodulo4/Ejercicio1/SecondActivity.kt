8d0dac91c573252b0d6af3e6acbedd86ea1ee2d4
JuanGR-FI <juan60430@hotmail.com>
2023-06-23 22:12:17 -0600

    Iniciando componentes graficos

--- app/src/main/java/com/jacgr/ejerciciosmodulo4/Ejercicio1/SecondActivity.kt
+++ app/src/main/java/com/jacgr/ejerciciosmodulo4/Ejercicio1/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.jacgr.ejerciciosmodulo4.Ejercicio1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import com.jacgr.ejerciciosmodulo4.R
@@ -11,6 +12,8 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val name = intent.getStringExtra("EXTRA_NAME")
         val lastName = intent.getStringExtra("EXTRA_LASTNAME")
@@ -46,5 +49,29 @@ class SecondActivity : AppCompatActivity() {
             finish()
         }
 
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                val returnIntent = Intent().apply {
+                    //putExtra("EXTRA_IS_OK", true)
+                    putExtra("EXTRA_NAME", "Juan")
+                    putExtra("EXTRA_LASTNAME", "Cervantes")
+                    putExtra("EXTRA_AGE", 23)
+                    putExtra("EXTRA_WEIGHT", 75.0)
+                    putExtra("EXTRA_MARRIED", true)
+                }
+                setResult(RESULT_OK, returnIntent)
+                onBackPressedDispatcher.onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
 }
