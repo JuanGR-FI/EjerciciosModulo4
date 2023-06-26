ae6af0cf19a19f9e4ddff2b0954d6005415dc47e
JuanGR-FI <juan60430@hotmail.com>
2023-06-25 22:50:24 -0600

    Creando Activities para registro y mostrar info

---
+++ app/src/main/java/com/jacgr/ejerciciosmodulo4/tarearegistro/InfoActivity.kt
@@ -0,0 +1,12 @@
+package com.jacgr.ejerciciosmodulo4.tarearegistro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jacgr.ejerciciosmodulo4.R
+
+class InfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
